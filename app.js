5bac4f7bc9f4423b86cbbb30d65a0c97fe5a9044
Carodarc <146771729+Carodarc@users.noreply.github.com>
2024-02-24 10:20:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-const path =require('path');
+const path = require('path');
+const publicPath = path.join(__dirname, 'public');
 
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.listen(port, ()=>{
     console.log(`Servidor corriendo en http://localhost:${port}`)
@@ -11,8 +12,8 @@ app.listen(port, ()=>{
 
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'src', 'views', 'home.html'))
+    res.sendFile(path.join(__dirname, 'src', 'views', 'home.html'));
 })
 
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(publicPath));
