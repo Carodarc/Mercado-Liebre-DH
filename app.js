3526ab262c56c1c38c0778024dfab874a0a8a50b
FernandoDib23 <fernandodib@gmail.com>
2024-03-20 10:00:06 -0300

    app and home

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-const publicPath = path.join(__dirname, 'public');
+app.use(express.static(path.join(__dirname,'src', 'public')));
 
-const port = 3001;
-
+const port = 3001
 app.listen(port, ()=>{
     console.log(`Servidor corriendo en http://localhost:${port}`)
-})
+});
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, 'src', 'views', 'home.html'));
 })
 
-app.use(express.static(publicPath));
