086ec1b6d59eea344941d9e42b95b019bebb8f04
FernandoDib23 <fernandodib@gmail.com>
2024-03-21 12:23:52 -0300

    mercado liebre

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-const publicPath = path.join(__dirname, 'src', 'public');
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
 const port = 3001;
+
 app.listen(port, ()=>{
     console.log(`Servidor corriendo en http://localhost:${port}`)
 });
@@ -13,3 +14,22 @@ app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, 'src', 'views', 'home.html'));
 })
 
+app.get('/register',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'register.html'))
+  
+  });
+  
+  app.get('/login',(req, res)=>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'login.html'))
+    
+  });
+  
+  app.get('*', (req, res)=>{
+      res.send(`
+      <div style="display:flex; flex-direction:column; align-items:center">
+      <h1>Esta pagina no existe</h1>
+      <img style="width:50%" src="/img/not-found.webp" alt="error-404">
+      </div>  
+      `)
+    })
+   
